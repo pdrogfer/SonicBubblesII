ce43f16864c8240d22ba4c55361d44f0ad1c8eb3
Pedro Gonzalez <pdro74@hotmail.com>
2014-01-30 11:00:27 +0100

    Transition between Intro and Game activities works now.

--- src/pgf/sonicbubblesii/IntroActivity.java
+++ src/pgf/sonicbubblesii/IntroActivity.java
@@ -2,14 +2,23 @@ package pgf.sonicbubblesii;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class IntroActivity extends Activity {
+public class IntroActivity extends Activity implements OnClickListener {
+	
+	Button startGame;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_intro);
+		
+		startGame = (Button) findViewById(R.id.btnStartGame);
+		startGame.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +26,18 @@ public class IntroActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.intro, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btnStartGame:
+			// launch game activity
+			Intent intent = new Intent(this, GameActivity.class);
+			startActivity(intent);
+			break;
+		}
+		
 	}
 
 }
