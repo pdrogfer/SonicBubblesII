a2eae48e69c22950ba85cfa1aec38e4598bd474e
pdrogfer <pdro74@hotmail.com>
2014-02-12 14:32:36 +0100

    commit to add the Theme class

---
+++ src/pgf/sonicbubblesii/Theme.java
@@ -0,0 +1,16 @@
+package pgf.sonicbubblesii;
+
+public class Theme {
+
+	// variables
+	int numDots, numSamples;
+	
+	// constructors
+	public Theme(int nDots, int nSamples) {
+		this.numDots = nDots;
+		this.numSamples = nSamples;
+		
+	}
+	
+	// methods
+}
