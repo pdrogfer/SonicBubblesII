d93c2707e7eb566552d2c84b40930a12226ee2b0
Pedro Gonzalez <pdro74@hotmail.com>
2014-02-21 11:11:44 +0100

    Added menu for intro and game activities

--- src/pgf/sonicbubblesii/GameActivity.java
+++ src/pgf/sonicbubblesii/GameActivity.java
@@ -45,7 +45,7 @@ public class GameActivity extends Activity implements OnClickListener {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.game, menu);
+		getMenuInflater().inflate(R.menu.general_menu, menu);
 		return true;
 	}
 
