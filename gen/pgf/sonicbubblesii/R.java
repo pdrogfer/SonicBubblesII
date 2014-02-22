b4969604eef5a618069ed2596b2d9f439c8f0f4e
pdrogfer <pdro74@hotmail.com>
2014-02-22 13:52:16 +0100

    added def land layout

--- gen/pgf/sonicbubblesii/R.java
+++ gen/pgf/sonicbubblesii/R.java
@@ -25,23 +25,16 @@ public final class R {
         public static final int logo_original=0x7f020001;
     }
     public static final class id {
-<<<<<<< HEAD
-        public static final int best_scores=0x7f090007;
-=======
-        public static final int action_settings=0x7f090008;
+        public static final int best_scores=0x7f090009;
         public static final int btnHowToPlay=0x7f090006;
->>>>>>> ui_fragments
         public static final int btnListenAgain=0x7f090000;
         public static final int btnNewGame=0x7f090002;
         public static final int btnStartGame=0x7f090005;
         public static final int drawing=0x7f090001;
-<<<<<<< HEAD
-        public static final int exit=0x7f090009;
-        public static final int hot_to_play=0x7f090008;
-        public static final int new_game=0x7f090006;
-=======
+        public static final int exit=0x7f09000b;
+        public static final int hot_to_play=0x7f09000a;
         public static final int intro_image=0x7f090004;
->>>>>>> ui_fragments
+        public static final int new_game=0x7f090008;
         public static final int tVwTitle=0x7f090003;
         public static final int textView1=0x7f090007;
     }
@@ -71,21 +64,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-<<<<<<< HEAD
         public static final int best_scores=0x7f060004;
+        public static final int btn_start_game=0x7f060008;
         public static final int exit=0x7f060006;
         public static final int feedback=0x7f060003;
         public static final int how_to_play=0x7f060005;
         public static final int new_game=0x7f060007;
+        public static final int sonic_bubbles_logo=0x7f060009;
         public static final int title_activity_game=0x7f060002;
-=======
-        public static final int btn_start_game=0x7f060005;
-        public static final int feedback=0x7f060004;
-        public static final int hello_world=0x7f060002;
-        public static final int how_to_play=0x7f060006;
-        public static final int sonic_bubbles_logo=0x7f060007;
-        public static final int title_activity_game=0x7f060003;
->>>>>>> ui_fragments
     }
     public static final class style {
         /** 
