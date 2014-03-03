c6e8b4747a4c4dcc87b4928a81854ff2ac3dd422
pdrogfer <pdro74@hotmail.com>
2014-03-03 19:05:19 +0100

    UI: better buttons and general design for != sizes

--- gen/pgf/sonicbubblesii/R.java
+++ gen/pgf/sonicbubblesii/R.java
@@ -32,7 +32,8 @@ public final class R {
         public static final int fondo1=0x7f020000;
         public static final int htc248=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int logo_original=0x7f020003;
+        public static final int intro_logo=0x7f020003;
+        public static final int logo_original=0x7f020004;
     }
     public static final class id {
         public static final int best_scores=0x7f0a0010;
