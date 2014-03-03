d581f81f31e46b634d017a7de371899070598e63
pdrogfer <pdro74@hotmail.com>
2014-03-03 18:19:58 +0100

    UI update

--- gen/pgf/sonicbubblesii/R.java
+++ gen/pgf/sonicbubblesii/R.java
@@ -30,12 +30,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int fondo1=0x7f020000;
-        public static final int fondo2=0x7f020001;
-        public static final int htc156=0x7f020002;
-        public static final int htc254=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int logo_original=0x7f020005;
-        public static final int our_planet2=0x7f020006;
+        public static final int htc248=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int logo_original=0x7f020003;
     }
     public static final class id {
         public static final int best_scores=0x7f0a0010;
